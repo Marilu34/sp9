882aee16414462855aa6f06117ef9890a814bbba
Ненахов Евгений Валентинович <yvnenak2@mts.ru>
2023-04-21 16:53:46 +0300

    fix structure

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,9 +1,9 @@
 package org.example.model;
 
 import lombok.*;
-//import javax.validation.constraints.Email;
-//import javax.validation.constraints.NotBlank;
-//import javax.validation.constraints.Past;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Getter
@@ -11,12 +11,12 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class User {
     private Integer id;
-//    @Email(message = "email должно содержать символы или цифры")
+    @Email(message = "email должно содержать символы или цифры")
     private String email;
-//    @NotBlank(message = "логин должен содержать символы")
+    @NotBlank(message = "логин должен содержать символы")
     private String login;
     private String name;
-//    @Past(message = "дата рождения должна быть не будущей")
-//    @NonNull
+    @Past(message = "дата рождения должна быть не будущей")
+    @NonNull
     private LocalDate birthday;
 }
