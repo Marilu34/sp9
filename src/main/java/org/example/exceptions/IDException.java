aef0238cdd943de1f37fd71aedba7f477c331f24
Marilu34 <lulima@yandex.ru>
2023-04-24 21:44:44 +0300

    1

--- src/main/java/org/example/exceptions/IDException.java
+++ src/main/java/org/example/exceptions/IDException.java
@@ -3,5 +3,6 @@ package org.example.exceptions;
 public class IDException extends RuntimeException {
 
     public IDException(String message) {
-        super(message);}
+        super(message);
+    }
 }
